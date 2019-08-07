249665825e60368f022f3396df35a8078502d883
Francis Ilechukwu <francis94c@gmail.com>
2019-08-07 06:41:04 +0100

    Cascade all Extended Cores.

--- application/core/MY_Uri.php
+++ application/core/MY_Uri.php
@@ -1,9 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class MY_Uri extends CI_Uri {
-
-  public $appsegments = array();
-  public $apprsegments = array();
+declare(strict_types=1);
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class MY_Uri extends CI_URI {
+	
+	public $appsegments = array();
+	public $apprsegments = array();
+	
+	public function uri_string() {
+		return get_instance()->config->item("st_uri_string") ?? $this->uri_string;
+	}
+	public function set_uri_string(string $uri_string):void {
+		$this->uri_string = $uri_string;
+	}
+	
 }
-?>
