4afb747fb86f07959d774c93c96ef5231e226283
Francis Ilechukwu <francis94c@gmail.com>
2019-08-07 06:26:09 +0100

    Trying out Soemthing on Travis.

--- application/core/TEST_Uri.php
+++ application/core/TEST_Uri.php
@@ -5,4 +5,7 @@ class TEST_Uri extends CI_URI {
 	public function uri_string() {
 		return get_instance()->config->item("st_uri_string") ?? $this->uri_string;
 	}
+	public function set_uri_string(string $uri_string):void {
+		$this->uri_string = $uri_string;
+	}
 }
